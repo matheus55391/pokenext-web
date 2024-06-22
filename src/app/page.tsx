64a20350ecf7defcc80f801ea78457c4e52d04a6
matheus55391 <matheus.felipe55391@gmail.com>
2024-06-22 10:50:52 -0300

    fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-'use client'
 import { useQuery } from '@tanstack/react-query';
-import Header from '../components/Header';
-import SearchSection from '../components/SearchSection';
+import Header from '@/components/Header';
+import SearchSection from '@/components/SearchSection';
 import { pokeapiService } from '@/Services/PokeapiService';
 import PokemonListSection from '@/components/PokemonListSection';
 
