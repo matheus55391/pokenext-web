306271041127730eb9169748ae19bbee1d1ae252
matheus55391 <matheus.felipe55391@gmail.com>
2024-06-22 12:00:01 -0300

    pokemon list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 'use client';
-// import { useQuery } from '@tanstack/react-query';
 import Header from '@/components/Header';
 import SearchSection from '@/components/SearchSection';
-// import { pokeapiService } from '@/services/PokeapiService';
-// import PokemonListSection from '@/components/PokemonListSection';
+import { pokeapiService } from '@/services/PokeapiService';
+import { useQuery } from '@tanstack/react-query';
 
 const Home: React.FC = () => {
-  // const getPokemons = async () => {
-  //   return await pokeapiService.getPokemons({ offset: 0, limit: 20 });
-  // }
-  // const { data: pokemons, isLoading, error } = useQuery({
-  //   queryKey: ['getPokemons'],
-  //   queryFn: getPokemons,
-  // });
+  const getPokemons = async () => {
+    return await pokeapiService.getPokemons({ offset: 0, limit: 20 });
+  }
+  const { data: pokemons, isLoading, error } = useQuery({
+    queryKey: ['getPokemons'],
+    queryFn: getPokemons,
+  });
   return (
     <main className="min-h-screen flex flex-col">
       <Header />
